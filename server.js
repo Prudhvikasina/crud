994352a4f6b9726eabbaa8586afc1a0d57ab8133
name <prudhvi.kasina@gmail.com>
2022-05-12 18:48:31 +0530

    make it better

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/first-app'));
+app.use(express.static(__dirname + '/dist/crud'));
 app.get('/*', function(_req, res)
-    { res.sendFile(path.join(__dirname + '/dist/first-app')); });
+    { res.sendFile(path.join(__dirname + '/dist/crud')); });
 app.listen(process.env.PORT || 8000);
